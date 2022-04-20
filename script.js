773f97d48a5c5b85c2bad5ba88bb5008db281d2e
Hasan Atik <hsnatik5050@gmail.com>
2022-04-20 19:47:21 +0300

    js

---
+++ script.js
@@ -0,0 +1,53 @@
+document.querySelector('#secPepperoni').addEventListener('click', addPepperoni)
+
+document.querySelector('#secSosis').addEventListener('click', addSosis)
+
+document.querySelector('#secZeytin').addEventListener('click', addZeytin)
+
+document.querySelector('#secSogan').addEventListener('click', addSogan)
+
+document.querySelector('#secBiber').addEventListener('click', addBiber)
+
+document.querySelector('#secSebze').addEventListener('click', addSebze)
+
+document.querySelector('#secDomates').addEventListener('click', addDomates)
+
+document.querySelector('#secMantar').addEventListener('click', addMantar)
+
+document.querySelector('#secAnanas').addEventListener('click', addAnanas)
+
+function addPepperoni(){
+    document.querySelector('#pepperoni').classList.toggle('hidden')
+}
+
+function addSosis(){
+    document.querySelector('#sosis').classList.toggle('hidden')
+}
+
+function addZeytin(){
+    document.querySelector('#zeytin').classList.toggle('hidden')
+}
+
+function addSogan(){
+    document.querySelector('#sogan').classList.toggle('hidden')
+}
+
+function addBiber(){
+    document.querySelector('#biber').classList.toggle('hidden')
+}
+
+function addSebze(){
+    document.querySelector('#sebze').classList.toggle('hidden')
+}
+
+function addDomates(){
+    document.querySelector('#domates').classList.toggle('hidden')
+}
+
+function addMantar(){
+    document.querySelector('#mantar').classList.toggle('hidden')
+}
+
+function addAnanas(){
+    document.querySelector('#ananas').classList.toggle('hidden')
+}
